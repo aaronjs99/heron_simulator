54df66b70a90e62ac78c66592bb79e675d8a54fa
Aaron John Sabu <aaronjohnsabu1999@gmail.com>
2026-03-19 13:50:39 -0700

    Remove preview path visualization and refine local/global path rendering parameters and colors.

--- src/PathVisualizerPlugin.cpp
+++ src/PathVisualizerPlugin.cpp
@@ -21,12 +21,10 @@ public:
         scene_(nullptr),
         manual_object_(nullptr),
         global_path_object_(nullptr),
-        preview_path_object_(nullptr),
         scene_node_(nullptr),
         robot_visual_(nullptr),
         new_path_received_(false),
         new_global_path_received_(false),
-        new_preview_path_received_(false),
         initialized_(false),
         warned_waiting_for_ros_(false) {}
   virtual ~PathVisualizerPlugin() {
@@ -82,12 +80,6 @@ private:
     this->global_path_object_->setBoundingBox(Ogre::AxisAlignedBox::BOX_INFINITE);
     this->scene_node_->attachObject(this->global_path_object_);
 
-    // Create ManualObject for the higher-level mission preview path.
-    this->preview_path_object_ = scene_manager->createManualObject("PreviewPath_MO" + suffix);
-    this->preview_path_object_->setDynamic(true);
-    this->preview_path_object_->setBoundingBox(Ogre::AxisAlignedBox::BOX_INFINITE);
-    this->scene_node_->attachObject(this->preview_path_object_);
-
     // Sub to nav path (Local Plan)
     this->sub_ =
         this->nh_->subscribe("/move_base/TebLocalPlannerROS/local_plan", 1, &PathVisualizerPlugin::PathCallback, this);
@@ -97,11 +89,9 @@ private:
         "/move_base/GlobalPlanner/plan", 1, &PathVisualizerPlugin::GlobalPathCallback, this);
     this->global_fallback_sub_ = this->nh_->subscribe(
         "/move_base/NavfnROS/plan", 1, &PathVisualizerPlugin::GlobalPathCallback, this);
-    this->preview_sub_ =
-        this->nh_->subscribe("/oracle/viz_path", 1, &PathVisualizerPlugin::PreviewPathCallback, this);
 
     this->initialized_ = true;
-    ROS_INFO("[PathVisualizerPlugin] LOADED! preview=/oracle/viz_path local=/move_base/TebLocalPlannerROS/local_plan global=/move_base/GlobalPlanner/plan|/move_base/NavfnROS/plan");
+    ROS_INFO("[PathVisualizerPlugin] LOADED! local=/move_base/TebLocalPlannerROS/local_plan global=/move_base/GlobalPlanner/plan|/move_base/NavfnROS/plan");
   }
 
   void PathCallback(const nav_msgs::Path::ConstPtr& msg) {
@@ -114,12 +104,6 @@ private:
     std::lock_guard<std::mutex> lock(this->mutex_);
     this->global_path_ = *msg;
     this->new_global_path_received_ = true;
-  }
-
-  void PreviewPathCallback(const nav_msgs::Path::ConstPtr& msg) {
-    std::lock_guard<std::mutex> lock(this->mutex_);
-    this->preview_path_ = *msg;
-    this->new_preview_path_received_ = true;
   }
 
   void OnUpdate() {
@@ -170,13 +154,12 @@ private:
         }
       }
 
-      // --- 2. Geometry Generation ---
-      // Use Gazebo-native overlay materials. The previous RViz-style material
-      // name was not available in Gazebo, which could leave the path invisible.
-      this->manual_object_->begin("Gazebo/LightBlueLaser", Ogre::RenderOperation::OT_TRIANGLE_STRIP);
+      // Draw a wide translucent blue local-plan ribbon, with the thinner global
+      // line rendered above it for a Waymo-style layered path look.
+      this->manual_object_->begin("Gazebo/BlueTransparentOverlay", Ogre::RenderOperation::OT_TRIANGLE_STRIP);
 
-      const double z_offset = 0.30;
-      const double half_width = 0.55;
+      const double z_offset = 0.26;
+      const double half_width = 0.78;
 
       for (size_t i = 0; i < this->latest_path_.poses.size(); ++i) {
         ignition::math::Vector3d p_curr = GetWorldPos(i, path_is_local, robot_found, robot_pose_world);
@@ -204,23 +187,23 @@ private:
         auto v_right = to_local(v_right_world);
 
         this->manual_object_->position(v_left.X(), v_left.Y(), v_left.Z() + z_offset);
-        this->manual_object_->colour(0.08, 0.82, 1.0, 0.96);
+        this->manual_object_->colour(0.18, 0.52, 1.0, 0.28);
 
         this->manual_object_->position(v_right.X(), v_right.Y(), v_right.Z() + z_offset);
-        this->manual_object_->colour(0.08, 0.82, 1.0, 0.96);
+        this->manual_object_->colour(0.18, 0.52, 1.0, 0.28);
       }
 
       this->manual_object_->end();
     }
 
-    // --- 3. Global Path (Narrow Dark Blue Ribbon - 15% of local width) ---
+    // --- 3. Global Path (thin, more opaque blue line) ---
     this->global_path_object_->clear();
 
     if (this->global_path_.poses.size() >= 2) {
       this->global_path_object_->begin("Gazebo/BlueTransparentOverlay", Ogre::RenderOperation::OT_TRIANGLE_STRIP);
 
-      const double global_z_offset = 0.38;
-      const double global_half_width = 0.18;
+      const double global_z_offset = 0.34;
+      const double global_half_width = 0.10;
 
       // Transform from world to local anchor frame
       auto to_local = [&](ignition::math::Vector3d world_pt) {
@@ -254,61 +237,13 @@ private:
         auto v_right = to_local(v_right_world);
 
         this->global_path_object_->position(v_left.X(), v_left.Y(), global_z_offset);
-        this->global_path_object_->colour(0.02, 0.30, 0.98, 1.0);
+        this->global_path_object_->colour(0.18, 0.52, 1.0, 0.96);
 
         this->global_path_object_->position(v_right.X(), v_right.Y(), global_z_offset);
-        this->global_path_object_->colour(0.02, 0.30, 0.98, 1.0);
+        this->global_path_object_->colour(0.18, 0.52, 1.0, 0.96);
       }
 
       this->global_path_object_->end();
-    }
-
-    // --- 4. High-level mission preview path (gold ribbon) ---
-    this->preview_path_object_->clear();
-
-    if (this->preview_path_.poses.size() >= 2) {
-      this->preview_path_object_->begin("Gazebo/Gold", Ogre::RenderOperation::OT_TRIANGLE_STRIP);
-
-      const double preview_z_offset = 0.52;
-      const double preview_half_width = 0.30;
-
-      auto to_local = [&](ignition::math::Vector3d world_pt) {
-        return anchor_pose.Rot().Inverse() * (world_pt - anchor_pose.Pos());
-      };
-
-      for (size_t i = 0; i < this->preview_path_.poses.size(); ++i) {
-        const auto& msg_pose = this->preview_path_.poses[i].pose;
-        ignition::math::Vector3d p_curr(msg_pose.position.x, msg_pose.position.y, 0.0);
-
-        ignition::math::Vector3d p_next;
-        if (i + 1 < this->preview_path_.poses.size()) {
-          const auto& next_pose = this->preview_path_.poses[i + 1].pose;
-          p_next = ignition::math::Vector3d(next_pose.position.x, next_pose.position.y, 0.0);
-        } else if (i > 0) {
-          const auto& prev_pose = this->preview_path_.poses[i - 1].pose;
-          ignition::math::Vector3d p_prev(prev_pose.position.x, prev_pose.position.y, 0.0);
-          p_next = p_curr + (p_curr - p_prev);
-        } else {
-          p_next = p_curr + ignition::math::Vector3d(1, 0, 0);
-        }
-
-        ignition::math::Vector3d dir = (p_next - p_curr).Normalize();
-        ignition::math::Vector3d perp(-dir.Y(), dir.X(), 0);
-
-        ignition::math::Vector3d v_left_world = p_curr + perp * preview_half_width;
-        ignition::math::Vector3d v_right_world = p_curr - perp * preview_half_width;
-
-        auto v_left = to_local(v_left_world);
-        auto v_right = to_local(v_right_world);
-
-        this->preview_path_object_->position(v_left.X(), v_left.Y(), preview_z_offset);
-        this->preview_path_object_->colour(1.0, 0.78, 0.10, 0.98);
-
-        this->preview_path_object_->position(v_right.X(), v_right.Y(), preview_z_offset);
-        this->preview_path_object_->colour(1.0, 0.78, 0.10, 0.98);
-      }
-
-      this->preview_path_object_->end();
     }
   }
 
@@ -340,13 +275,9 @@ private:
   bool new_global_path_received_;
   ros::Subscriber global_sub_;
   ros::Subscriber global_fallback_sub_;
-  ros::Subscriber preview_sub_;
   Ogre::ManualObject* global_path_object_;
-  Ogre::ManualObject* preview_path_object_;
   bool initialized_;
   bool warned_waiting_for_ros_;
-  nav_msgs::Path preview_path_;
-  bool new_preview_path_received_;
 };
 
 // Register this plugin with the simulator
